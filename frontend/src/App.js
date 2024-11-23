77f41beecf8069223707305bc6ca1b9175f68eda
KiRT4P <81580109+KiRT4P@users.noreply.github.com>
2024-11-23 13:09:52 +0100

    setup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,27 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+//pages
+import Navbar from "./components/Navbar"
+import Footer from './components/Footer';
+import Home from "./pages/home";
+import User from './pages/User';
+import History from './pages/History';
 
-export default App;
+
+export default function App() {
+
+
+  return (
+    <BrowserRouter >
+      <main className='min-h-full flex flex-col w-screen overflow-x-hidden' >
+        <Navbar />
+        <Routes className=''>
+          <Route index element={<Home />} />
+          <Route path="user" element={<User />} />
+          <Route path="history" element={<History />} />
+        </Routes>
+        {/* <Footer /> */}
+      </main>
+    </BrowserRouter>
+
+  );
+}
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+    return (
+        <div className="w-screen h-12">
+            <div className="fixed h-12 w-screen bg-cyan-600 ">
+                <div className="flex justify-between max-w-[80vw] mx-auto items-center h-12 text-white">
+                    Hello world
+                </div>
+
+            </div>
+        </div>
+    )
+}
