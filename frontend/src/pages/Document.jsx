7c3eb2a933dc95b23da4679b0afe03809555525f
KiRT4P <81580109+KiRT4P@users.noreply.github.com>
2024-11-23 20:03:45 +0100

    changes in document page

--- frontend/src/pages/Document.jsx
+++ frontend/src/pages/Document.jsx
@@ -73,61 +73,68 @@ export default function Document() {
     return (
         <div className="page flex items-center">
             <div className='w-1/4 h-[90vh] '>
-                <div className='bg-terciary mx-4 rounded-2xl  flex flex-col justify-between py-8 items-center h-[90vh] '>
-                    <h1 className='text-2xl text-primary text-center mb-12'>New Report</h1>
-                    <form onSubmit={handleSubmit} className='mx-auto flex flex-col justify-between grow   w-[80%] ' >
+                <div className='bg-terciary mx-4 rounded-md   py-8 items-center h-[90vh] '>
+                    <h1 className='text-2xl text-primary text-center'>New Report</h1>
+                    <div className='h-full flex flex-col justify-center '>
+                        <form onSubmit={handleSubmit} className='grow mx-auto w-[80%] mt-4' >
 
-                        <div className='flex flex-col '>
-                            <label htmlFor="docnName" className='text-center text-lg font-semibold  '>Document Name:</label>
-                            <input className='bg-bg  rounded-xl px-4 py-2 text-white' type="text" id="docnName" name="docnName" value={formData.docnName} onChange={handleChange} />
-                        </div>
-                        <div className='flex flex-col justify-center'>
-                            <label label className='text-center text-lg font-semibold  mb-4' htmlFor="target">Target File</label>
-
-                            <div className="bg-bg  font-bold cursor-pointer py-4 rounded-xl flex flex-col justify-center items-center  " onClick={() => openFileExplorer(1)}>
-
-                                {formData.target ? <IconFileFilled size={32} color='#fff' /> : <IconFile size={32} color='#fff' />}
-                                {!!formData.target && <p className=' text-sm text-center'>{formData.target.name}</p>}
-                                {pending && <p className=' text-sm'>Uploading...</p>}
-                                <input
-                                    type="file"
-                                    ref={targetInputRef}
-                                    onChange={e => handleFileChange(e, 1)}
-                                    className='hidden'
-                                />
+                            <div className='flex flex-col my-4 '>
+                                <label htmlFor="docnName" className='text-center text-lg font-semibold mb-1'>Document Name</label>
+                                <input className='bg-bg  rounded-md px-4 py-2 text-white' type="text" id="docnName" name="docnName" value={formData.docnName} onChange={handleChange} />
                             </div>
-                        </div>
-                        <div className=' flex flex-col  justify-center'>
-                            <label className='text-center text-lg font-semibold mb-4' htmlFor="rules">Rules File</label>
-                            <div className="bg-bg  font-bold cursor-pointer py-4   rounded-xl flex flex-col justify-center items-center  " onClick={() => openFileExplorer(2)}>
+                            <div className='flex flex-col justify-center my-4'>
+                                <label label className='text-center text-lg font-semibold mb-1' htmlFor="target">Target File</label>
 
-                                {formData.rules ? <IconFileFilled size={32} color='#fff' /> : <IconFile size={32} color='#fff' />}
-                                {!!formData.rules && <p className=' text-sm text-center'>{formData.rules.name}</p>}
-                                {pending && <p className=' text-sm'>Uploading...</p>}
+                                <div className="bg-bg  font-bold cursor-pointer py-4 rounded-md flex flex-col justify-center items-center  " onClick={() => openFileExplorer(1)}>
 
-                                <input
-                                    type="file"
-                                    ref={rulesInputRef}
-                                    onChange={e => handleFileChange(e, 2)}
-                                    className='hidden'
-                                />
+                                    {formData.target ? <IconFileFilled size={32} color='#fff' /> : <IconFile size={32} color='#fff' />}
+                                    {!!formData.target && <p className=' text-sm text-center'>{formData.target.name}</p>}
+                                    {pending && <p className=' text-sm'>Uploading...</p>}
+                                    <input
+                                        type="file"
+                                        ref={targetInputRef}
+                                        onChange={e => handleFileChange(e, 1)}
+                                        className='hidden'
+                                    />
+                                </div>
                             </div>
-                        </div>
-                        <div className='w-full   flex justify-center items-center'>
-                            <div className={`btn w-full  text-center text-xl ${pending ? " cursor-not-allowed " : " cursor-pointer "}`} onClick={handleSubmit}>{pending ? "Uploading..." : "Submit"}</div>
-                        </div>
-                    </form>
+                            <div className=' flex flex-col  justify-center my-4'>
+                                <label className='text-center text-lg font-semibold mb-1' htmlFor="rules">Rules File</label>
+                                <div className="bg-bg  font-bold cursor-pointer py-4   rounded-md flex flex-col justify-center items-center  " onClick={() => openFileExplorer(2)}>
+
+                                    {formData.rules ? <IconFileFilled size={32} color='#fff' /> : <IconFile size={32} color='#fff' />}
+                                    {!!formData.rules && <p className=' text-sm text-center'>{formData.rules.name}</p>}
+                                    {pending && <p className=' text-sm'>Uploading...</p>}
+
+                                    <input
+                                        type="file"
+                                        ref={rulesInputRef}
+                                        onChange={e => handleFileChange(e, 2)}
+                                        className='hidden'
+                                    />
+                                </div>
+                            </div>
+
+                            <div className='w-full   flex justify-center items-center mt-8'>
+                                <div className={`btn w-full  text-center text-xl ${pending ? " cursor-not-allowed " : " cursor-pointer "}`} onClick={handleSubmit}>{pending ? "Uploading..." : "Submit"}</div>
+                            </div>
+
+                        </form>
+                    </div>
 
 
                 </div>
 
             </div >
             <div className='w-3/4 h-[90vh]'>
-                <div className='bg-terciary mx-4 rounded-2xl p-16 h-[90vh]'>
-                    <div className='flex justify-center items-center w-full h-full'>
+                <div className='bg-terciary mx-4 rounded-md p-16 h-[90vh]'>
+                    {!resolved && <div className='flex justify-center items-center w-full h-full'>
                         {!pending && <h1 className='text-primary text-3xl'>Submit files to create new report</h1>}
                         {pending && <h1 className='text-primary text-3xl'>Generating...</h1>}
-                    </div>
+                    </div>}
+                    {resolved && <div className='flex justify-center items-center w-full h-full'>
+                        <h1 className='text-primary text-3xl'>Report Generated</h1>
+                    </div>}
 
                 </div>
 
